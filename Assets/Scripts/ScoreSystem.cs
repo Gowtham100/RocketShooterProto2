546fc6045aa0deff49175a14e3bcc451480f0065
Gowtham100 <gowtham100mohan@gmail.com>
2016-06-20 09:56:44 -0800

    fixed 3

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -6,13 +6,13 @@ public class ScoreSystem : MonoBehaviour {
     public Text scoreText;
     public int initialNumber;
     public int score;
-    public GameObject win;
+
     public GameObject Asteroid;
 
 
 	// Use this for initialization
 	void Start () {
-        win.SetActive(false);
+       
         initialNumber = transform.childCount;
     }
 	
@@ -21,17 +21,10 @@ public class ScoreSystem : MonoBehaviour {
         score = initialNumber - transform.childCount;
         scoreText.text = "Score: " + score.ToString();
 
-        winner();
+       
 
        
     }
 
-    void winner()
-    {
-        if (score == transform.childCount)
-        {
-            win.SetActive(true);
-            Destroy(Asteroid);
-        }
-    }
+   
 }
